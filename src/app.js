cf77e6bd0624fce712517eeba6ea9218c97cea9e
Lavdeep Singh <lovedeepsingh123.lk@gmail.com>
2021-09-09 21:24:35 +0530

    added compression middleware

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const helmet=require('helmet');
 const cors = require('cors');
+const compression=require('compression');
 
 require('dotenv').config();
 
@@ -13,6 +14,8 @@ const app = express();
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
+
+app.use(compression());
 
 app.get('/', (req, res) => {
   res.json({
